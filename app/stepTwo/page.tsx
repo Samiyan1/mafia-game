4689747a938e837ad382fd7a7371598d5140ebf1
samiyan <sadas@gmail.com>
2023-10-29 16:19:44 +0330

    debug for deploy

--- app/stepTwo/page.tsx
+++ app/stepTwo/page.tsx
@@ -14,38 +14,37 @@ function Page() {
 
 
   return (
-    <main className="h-auto justify-start relative  flex flex-col items-center">
+    <main className="h-screen justify-start relative  flex flex-col items-center">
       <Header value1='و' value2=' سناری' value3='اب' value4='خ' value5='انت' />
 
       <div className='flex flex-wrap justify-evenly  mt-2 overflow-y-scroll h-auto'>
         {MafiaScenarios && MafiaScenarios.map((item: any, index: number) => {
-          // {`./stepTwo/${item.title}`}
-          // key={index} 
+       
           return (
-            <div className='card-scenario bg-black mt-2 w-[47vw] h-[40vh] flex-col flex justify-start items-center '>
-              <Image
-                src={item.scenarioImage}
-                alt="image of scenario card"
-                height={0}
-                draggable='false'
-                className='rounded-[15px]'
-                width={0}
-                sizes="80vw"
-                style={{
-                  width: '100%',
-                  height: '80%',
-                }}
-              />
-              <div className=' mb-2 flex justify-between flex-col'>
-                <div className='title text-white'>{item.title}</div>
-                <div className='descriptaion'>{item.scenarioDecription}</div>
-                <BtnMoreDetail action={() => { router.push(`/stepTwo/${item.ruleName}`) }} value={'more'} className='' />
+              <div key={index} onClick={()=> router.push(`./stepTwo/${item.title}`)} className='card-scenario bg-black mt-2 w-[47vw] h-[40vh] flex-col flex justify-start items-center '>
+                <Image
+                  src={item.scenarioImage}
+                  alt="image of scenario card"
+                  height={0}
+                  draggable='false'
+                  className=''
+                  width={0}
+                  sizes="80vw"
+                  style={{
+                    width: '100%',
+                    height: '70%',
+                  }}
+                />
+                <div className=' mb-2 flex justify-between flex-col'>
+                  <div className='title text-white'>{item.title}</div>
+                  <div className='descriptaion'>{item.scenarioDecription}</div>
+                  <BtnMoreDetail action={() => { router.push(`/stepTwo/${item.title}`) }} value={'more'} className='' />
+                </div>
               </div>
-            </div>
           )
         })}
       </div>
-      <Footer src='/logoWhite.png' className='' action={()=>{router.push('/')}} />
+      <Footer src='/logoWhite.png' className='' action={() => { router.push('/') }} />
 
     </main>
   )
