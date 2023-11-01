15c5e662a7078dda482c29dee93afa081ddf59c2
samiyan <sadas@gmail.com>
2023-11-01 15:07:37 +0330

    better ui

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -2,14 +2,10 @@ import React from 'react'
 import './header.css';
 export default function Header(props: { div?: any, action?: any, value1: string, value2: string, value3: string, value4: string, value5: string, className?: string }) {
     return (
-        <header className={`bg-black sticky top-0 z-30 w-full flex justify-evenly flex-row items-center ${props.className}`}>
-            <button onClick={props.action} className=" w-full flex justify-center">
-                <div className="header flex justify-evenly font-[Titr]">
-                    {props.div}
-                    <b>{props.value5}<span>{props.value4}</span>{props.value3}<span>{props.value2}</span>{props.value1}</b>
-                </div>
-
-            </button>
+        <header className={`header bg-black sticky top-0 z-30 w-full flex justify-evenly flex-row items-center ${props.className}`}>
+            <div className="logo">
+                <b>{props.value5}<span>{props.value4}</span>{props.value3}<span>{props.value2}</span>{props.value1}</b>
+            </div>
         </header>
 
     )
