e6cec1b05b1a89c8154d03443bed147ca7bba957
samiyan1 <mehdisamiyann@gmail.com>
2023-11-10 00:50:35 +0330

    debug for deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,12 @@ import { ToastContainer, toast } from 'react-toastify';
 const inter = Inter({ subsets: ['latin'] })
 import { NextUIProvider } from "@nextui-org/react";
 import Head from 'next/head'
-import type { AppProps } from "next/app";
+
+
+// export const metadata: Metadata = {
+//   title: 'mafia game',
+//   description: 'mafia game',
+// }
 
 export default function RootLayout({
   children,
@@ -15,81 +20,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <title>My awesome PWA app</title>
-        <meta name="description" content="Best PWA app in the world!" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="mask-icon" href="/icons/mask-icon.svg" color="#FFFFFF" />
-        <meta name="theme-color" content="#ffffff" />
-        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/icons/touch-icon-ipad.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/icons/touch-icon-iphone-retina.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="167x167"
-          href="/icons/touch-icon-ipad-retina.png"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="https://yourdomain.com" />
-        <meta name="twitter:title" content="My awesome PWA app" />
-        <meta name="twitter:description" content="Best PWA app in the world!" />
-        <meta name="twitter:image" content="/icons/twitter.png" />
-        <meta name="twitter:creator" content="@DavidWShadow" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="My awesome PWA app" />
-        <meta property="og:description" content="Best PWA app in the world!" />
-        <meta property="og:site_name" content="My awesome PWA app" />
-        <meta property="og:url" content="https://yourdomain.com" />
-        <meta property="og:image" content="/icons/og.png" />
-        {/* add the following only if you want to add a startup image for Apple devices. */}
-        <link
-          rel="apple-touch-startup-image"
-          href="/images/apple_splash_2048.png"
-          sizes="2048x2732"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/images/apple_splash_1668.png"
-          sizes="1668x2224"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/images/apple_splash_1536.png"
-          sizes="1536x2048"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/images/apple_splash_1125.png"
-          sizes="1125x2436"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/images/apple_splash_1242.png"
-          sizes="1242x2208"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/images/apple_splash_750.png"
-          sizes="750x1334"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/images/apple_splash_640.png"
-          sizes="640x1136"
-        />
-      </Head>
+    <html lang="en">
 
+      <Head>
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='apple-touch-icon' href='/icon.png' />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <NextUIProvider>
 
         <Provider store={store}>
@@ -108,7 +45,7 @@ export default function RootLayout({
         </Provider>
       </NextUIProvider>
 
-    </>
+    </html>
 
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
-const {
-	PHASE_DEVELOPMENT_SERVER,
-	PHASE_PRODUCTION_BUILD,
-  } = require("next/constants");
+const withPWA = require("@ducanh2912/next-pwa").default({
+	dest: "public",
+  });
   
-  /** @type {import("next").NextConfig} */
-  const nextConfig = {
-	reactStrictMode: true,
-  };
-  
-  module.exports = (phase) => {
-	if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-	  const withPWA = require("@ducanh2912/next-pwa").default({
-		dest: "public",
-	  });
-	  return withPWA(nextConfig);
-	}
-	return nextConfig;
-  };
+  module.exports = withPWA({
+	// Your Next.js config
+  });
